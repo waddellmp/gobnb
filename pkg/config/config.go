36b09ecfdd655d7f35b8456edebd4bae5d6fdfb6
Matthew Waddell <waddelmp08@gmail.com>
2024-03-05 22:19:42 -0600

    Create pkg var config to share with startup

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,12 @@
 package config
 
 // Configuration
-type AppConfig struct {
-	Port string
+type appConfig struct {
+	Port     string
+	UseCache bool
+}
+
+var AppConfig appConfig = appConfig{
+	Port:     ":8080",
+	UseCache: false,
 }
